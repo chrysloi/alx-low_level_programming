f9d9c0c68fb4efc90e193ad2ba852777a0276ac4
Chrysanthe <eloi.chrysanthe@gmail.com>
2022-04-13 19:40:20 +0200

    initial commit Function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
@@ -8,7 +7,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-    if (!name || !f)
-        return;
-    f(name);
+    if (name && f)
+        f(name);
 }
