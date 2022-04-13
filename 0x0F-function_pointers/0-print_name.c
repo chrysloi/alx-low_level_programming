c29fe9d6dba230bd359ce712fb28b3352d001f67
Chrysanthe <eloi.chrysanthe@gmail.com>
2022-04-13 19:48:21 +0200

    initial commit Function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,11 @@
 #include "function_pointers.h"
 
 /**
- * print_name - will print a name
- * @name: name to be printed
- * @f: pointer to the printing function
- * 
- * Return: no return
+ * print_name - prints a name.
+ * @name: input name.
+ * @f: function pointer.
+ *
+ * Return: no return.
  */
 void print_name(char *name, void (*f)(char *))
 {
